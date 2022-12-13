f67124ed725e854765c4e37fe4fd2d50db1debad
Bill Zhuo <w3zhuo@uwaterloo.ca>
2022-12-13 00:04:57 -0500

    simplex in haskell from scratch

--- ParBnC/ParBnC/SimpleLP.hs
+++ ParBnC/ParBnC/SimpleLP.hs
@@ -6,6 +6,9 @@ import Numeric.LinearProgramming
       Constraints(Sparse, Dense),
       Optimization(Maximize, Minimize),
       Solution, Bounds, Bound( (:&:) ))
+import Numeric.IEEE ( IEEE(infinity) )
+import Data.List
+import Numeric.LinearAlgebra as LA
 
 prob :: Optimization
 prob = Maximize [4, -3, 2, 0, 0]
@@ -51,6 +54,75 @@ branchAndBoundSolve :: Problem -> Solution
 branchAndBoundSolve x@Problem {problemType = LP, ..} = solveProblem x
 
 -- for simple LP test
+
+
+toTableau :: LA.Vector R -> LA.Matrix R -> LA.Vector R -> LA.Matrix R
+toTableau costC matA constB = tab where 
+    xb = LA.fromColumns $ LA.toColumns matA ++ [constB]
+    z = mappend costC $ vector [0]
+    tab = LA.fromRows $ LA.toRows xb ++ [z]
+
+isImprovable :: LA.Matrix R -> Bool
+isImprovable tab = any (> 0) $ LA.toList cost where 
+    cost = subVector 0 (cols tab - 1) $ tab ! (rows tab - 1)
+
+getPivotPosition :: LA.Matrix R -> (Int, Int)
+getPivotPosition tab = (row, column) where 
+    z = tab ! (rows tab - 1)
+    cost = subVector 0 (cols tab - 1) z
+    column = head $ LA.find (>0) cost
+    getElem rowEq
+        | elem <= 0 = infinity::R
+        | otherwise = (rowEq ! (LA.size rowEq - 1)) / elem
+        where 
+            elem = rowEq ! column
+    restrictions = map getElem $ init (LA.toRows tab)
+    Just row =  elemIndex (minimum restrictions) restrictions
+
+pivotStep :: Matrix R -> (Int, Int) -> Matrix R
+pivotStep tab (row, column) = newTableau where
+    pivotVal = (tab ! row) ! column
+    newPivotRow = cmap (/ pivotVal) $ tab ! row
+    updateRow rowIdx rowEq 
+        | rowIdx == row = newPivotRow
+        | otherwise = rowEq - newPivotRow * LA.scalar (rowEq ! column)
+    newTableau = LA.fromRows $ zipWith updateRow [0..] $ LA.toRows tab
+
+isBasic :: Vector R -> Bool
+isBasic colVec = (LA.sumElements colVec == 1) && (length zeroVec == colVecLen) where
+    zeroVec = filter (== 0) $ LA.toList colVec
+    colVecLen = LA.size colVec - 1
+
+getSolution :: Matrix R -> Vector R
+getSolution tab = solution where 
+    colSize = LA.cols tab
+    columns = LA.toColumns $ LA.takeColumns (colSize - 1) tab
+    lastCol = LA.flatten $ LA.dropColumns (colSize - 1) tab
+    findSol colVec
+        | isBasic colVec = sol 
+        | otherwise = 0::R where
+            oneIndex = head $ LA.find (==1) colVec
+            sol = lastCol ! oneIndex
+    solution = LA.fromList $ map findSol columns
+
+simplexWithTab :: LA.Vector R -> LA.Matrix R -> LA.Vector R -> LA.Vector R
+simplexWithTab costC matA constB = solution where 
+    tab = toTableau costC matA constB
+    updateTab subTab
+        | isImprovable subTab = subTab
+        | otherwise = updateTab newTab where 
+            pivotPos = getPivotPosition subTab
+            newTab = pivotStep subTab pivotPos
+    lastTab = updateTab tab
+    solution = getSolution lastTab
+
+b :: Vector R
+b = LA.fromList [2,4,4::R]
+matA :: Matrix R
+matA = LA.fromLists [[-1,1,1,0,0],[1,0,0,1,0],[0,1,0,0,1::R]]
+c :: Vector R
+c = fromList [1,1,0,0,0::R]
+
 
 problemTest :: Problem
 problemTest = Problem { 
